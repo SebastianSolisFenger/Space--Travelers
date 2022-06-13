b4ede792d71aaa0bb3b742566b045f2f1e21f8be
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-13 16:37:18 -0300

    Import Mission on APP and add some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 // import './index.css';
+import Missions from './components/Missions';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Missions />
     </div>
   );
 }
