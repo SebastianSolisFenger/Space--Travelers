2e05a7ab6fcf9a9203c3f094a99ca632b68f8ccf
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-14 16:53:01 -0300

    Change data fetching to be executed on first load as required

--- src/components/AllRockets.js
+++ src/components/AllRockets.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import Rocket from './Rocket';
 import './AllRockets.css';
-import { getRocketList } from '../redux/rockets/rockets';
 
 const AllRockets = () => {
-  const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rocketsReducer);
-
-  useEffect(() => {
-    dispatch(getRocketList());
-  }, [dispatch]);
 
   return rockets.map((rocket) => (
     <div key={rocket.id}>
