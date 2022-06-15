167690b0bb683e86afde0cbf184e58d1d320d3c4
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-06-15 16:15:23 -0300

    Remove comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-// import './index.css';
-// import Missions from './components/Missions';
-// import AllRockets from './components/AllRockets';
 import routes from './routes';
 
 function App() {
@@ -14,8 +11,6 @@ function App() {
           <Route path={route.path} element={route.element} key={route.id} />
         ))}
       </Routes>
-      {/* <AllRockets />
-      <Missions /> */}
     </div>
   );
 }
